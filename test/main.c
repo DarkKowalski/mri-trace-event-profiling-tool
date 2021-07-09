c2c7fcf52b3f1b29f0929c0689bdb1caa32527f2
Kowalski Dark <darkkowalski2012@gmail.com>
2021-07-09 16:16:14 +0800

    Save

--- test/main.c
+++ test/main.c
@@ -3,9 +3,16 @@
 void *function0(void *args)
 {
     int ractor_id = ractor_init_profiling_event_list();
-    printf("Ractor ID = %d\n", ractor_id);
     int id = trace_profiling_event_begin();
     sleep(1);
+    trace_profiling_event_end(id);
+}
+
+void *function1(void *args)
+{
+    int ractor_id = ractor_init_profiling_event_list();
+    int id = trace_profiling_event_begin();
+    sleep(3);
     trace_profiling_event_end(id);
 }
 
@@ -15,8 +22,10 @@ int main(void)
     init_profiling_event_bucket();
 
     pthread_t thread_id[8];
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 4; i++)
         pthread_create(&thread_id[i], NULL, function0, NULL);
+    for (int i = 4; i < 8; i++)
+        pthread_create(&thread_id[i], NULL, function1, NULL);
     for (int i = 0; i < 8; i++)
         pthread_join(thread_id[i], NULL);
 
